8b9e26aea55ea4987026ab6dd0aa43f928d084be
Er1ck-Esp1n0sa <zs20007628@estudiantes.uv.mx>
2023-08-30 09:11:40 -0600

    conexion de php

--- PHP-Apache/index.php
+++ PHP-Apache/index.php
@@ -1,1 +1,17 @@
-<?php echo "Hola mundo"; ?>
+<?php
+$host = 'localhost'; // Cambia esto si el contenedor se ejecuta en otra máquina
+$port = '5432';
+$dbname = 'nombre_de_la_base_de_datos';
+$user = 'postgres'; // Usuario por defecto de PostgreSQL
+$password = 'contraseña'; // La contraseña que configuraste
+
+$dsn = "pgsql:host=$host;port=$port;dbname=$dbname;user=$user;password=$password";
+
+try {
+    $pdo = new PDO($dsn);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Conexión exitosa a la base de datos!";
+} catch (PDOException $e) {
+    echo "Error de conexión: " . $e->getMessage();
+}
+?>
